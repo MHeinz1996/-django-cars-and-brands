e8d1341686a9b82f78cd17fe9acc8fce20b3fb36
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-11 15:59:38 -0500

    Completed cars lists. Need to work on edit functions

--- car_app/cars_and_brands/urls.py
+++ car_app/cars_and_brands/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('brands/', views.brands),
     path('brands/<int:brand_id>/', views.brands_id),
+    path('brands/<int:brand_id>/cars/', views.cars),
+    path('brands/<int:brand_id>/cars/<int:car_id>/', views.cars_id),
 ]
--- car_app/cars_and_brands/views.py
+++ car_app/cars_and_brands/views.py
@@ -4,13 +4,7 @@ from .models import Brand, Car
 
 # Create your views here.
 def index(request):
-    data = []
-    brands = Brand.objects.all()
-    for brand in brands:
-        print(brand.name)
-        data.append({'name': brand.name})
-    print(data)
-    return render(request, 'cars_and_brands/index.html', {'data': data})
+    return render(request, 'cars_and_brands/index.html')
 
 def brands(request):
     data = []
@@ -25,3 +19,18 @@ def brands_id(request, brand_id):
     brand = Brand.objects.get(id=brand_id)
     data = {'name': brand.name, 'id': brand_id}
     return render(request, 'cars_and_brands/brands_id.html', data)
+
+def cars(request, brand_id):
+    data = []
+    cars = Car.objects.all()
+    for car in cars:
+        if car.brand.id == brand_id:
+            print(car.brand.name)
+            data.append({'name': car.name, 'id': car.id, 'brand': car.brand.name, 'brand_id': car.brand.id})
+    # print(cars)
+    return render(request, 'cars_and_brands/cars.html', {'data': data})
+
+def cars_id(request, brand_id, car_id):
+    car = Car.objects.get(id=car_id)
+    data = {'name': car.name, 'id': car.id}
+    return render(request, 'cars_and_brands/car_id.html', data)
