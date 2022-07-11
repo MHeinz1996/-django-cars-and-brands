044556df7d18f43b3af49b45dc677edc8988a38a
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-11 15:11:03 -0500

    Was able to get data from database and display it to the front end

--- car_app/cars_and_brands/views.py
+++ car_app/cars_and_brands/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Brand, Car
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    data = []
+    brands = Brand.objects.all()
+    for brand in brands:
+        print(brand.name)
+        data.append({'name': brand.name})
+    print(data)
+    return render(request, 'cars_and_brands/index.html', {'data': data})
+
+def brands(request):
+    data = []
+    brands = Brand.objects.all()
+    for brand in brands:
+        print(brand.name)
+        data.append({'name': brand.name})
+    print(data)
+    return render(request, 'cars_and_brands/brands.html', {'data': data})
