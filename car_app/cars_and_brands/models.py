dd3769f5904e43eaaa7a086e59943c2296281fbd
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-11 16:07:13 -0500

    Updated models so that names for brands and cars must be unique

--- car_app/cars_and_brands/models.py
+++ car_app/cars_and_brands/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Brand(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
 class Car(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     brand = models.ForeignKey(Brand, on_delete=CASCADE)
